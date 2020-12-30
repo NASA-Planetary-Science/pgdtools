02bfae518b0168209af6c2d02a7baf95b0dfd084
Reto Trappitsch <reto@galactic-forensics.space>
2020-12-30 15:15:20 -0500

    Setup of project - analogue to iniabu for now
    Might want to go to poetry for more consistent dependency management...

---
+++ noxfile.py
@@ -0,0 +1,70 @@
+"""Configuration file for testing the package with ``nox``."""
+
+import nox
+
+
+nox.options.sessions = "lint", "safety", "tests", "xdoctest"
+
+package = "stardustlib"
+locations = "stardustlib", "tests", "noxfile.py", "docs/conf.py"
+python_suite = ["3.9", "3.8", "3.7", "3.6"]
+python_main = "3.9"
+
+
+@nox.session(python=python_main)
+def black(session):
+    """Autoformat all python files with black."""
+    args = session.posargs or locations
+    session.install("black==20.8b1")
+    session.run("black", *args)
+
+
+@nox.session(python=python_main)
+def build(session):
+    """Pack stardustlib for release on PyPi."""
+    session.install("flit")
+    session.run("flit", "build")
+
+
+# @nox.session(python=python_main)
+# def docs(session):
+#     """Build the documentation."""
+#     session.install("sphinx", "sphinx_rtd_theme", "-r", "requirements.txt", "pytest")
+#     session.chdir("docs")
+#     session.run(
+#         "sphinx-build", "-b", "html", ".", "_build/html/"
+#     )  # as for readthedocs.io
+
+
+@nox.session(python=python_main)
+def lint(session):
+    """Lint project using ``flake8``."""
+    args = session.posargs or locations
+    session.install("-r", "dev-requirements.txt")
+    session.run("flake8", *args)
+
+
+@nox.session(python=python_suite)
+def tests(session):
+    """Test the project using ``pytest``."""
+    session.install("-r", "requirements.txt", "-r", "dev-requirements.txt")
+    session.run("pytest")
+
+
+@nox.session(python=python_main)
+def safety(session):
+    """Safety check for all dependencies."""
+    session.install("safety", "-r", "requirements.txt", "-r", "dev-requirements.txt")
+    session.run(
+        "safety",
+        "check",
+        "--full-report",
+    )
+
+
+@nox.session(python=python_main)
+def xdoctest(session):
+    """Test docstring examples with xdoctest."""
+    args = session.posargs or ["all"]
+    session.install("xdoctest[all]", "-r", "requirements.txt")
+    session.run("python", "-m", "xdoctest", package, *args)
---
+++ stardustlib/__init__.py
@@ -0,0 +1,17 @@
+"""Package to interact with the stardust database."""
+
+# import the standard module
+# from .main import IniAbu
+
+# Package information
+__version__ = "0.0.1"
+
+__title__ = "stardustlib"
+__description__ = "Read and interact with the stardust library from within python."
+
+__uri__ = "https://stardustlib.readthedocs.io/en/latest/"
+__author__ = "Reto Trappitsch"
+__email__ = "reto@galactic-forensics.space"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020, Reto Trappitsch"
