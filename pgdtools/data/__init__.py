1cb981d2e0be431d148b51776a771c8d6cff28ec
Reto Trappitsch <reto@galactic-forensics.space>
2023-07-27 17:05:27 +0200

    Remove old dbs, add DB file url for json

--- pgdtools/data/__init__.py
+++ pgdtools/data/__init__.py
@@ -2,6 +2,8 @@
 
 from pathlib import Path
 
+DB_FILE_URL = "https://raw.githubusercontent.com/NASA-Planetary-Science/pgdtools/main/database/db.json"
+
 BIBFILE = Path(__file__).parent.joinpath("pgd_references.bib").absolute()
 OLAFKEYFILE = (
     Path(__file__).parent.joinpath("pgd_library_id_doi_olafkey.csv").absolute()
