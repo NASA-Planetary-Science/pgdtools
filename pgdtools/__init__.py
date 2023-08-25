3a13297370534a26e35ce288606cc997f30450d9
Reto Trappitsch <reto@galactic-forensics.space>
2023-08-25 15:05:52 +0200

    import version from _version

--- pgdtools/__init__.py
+++ pgdtools/__init__.py
@@ -1,17 +1,15 @@
 """Package to interact with the presolar grain database."""
 
-from setuptools_scm import get_version
-
 from . import data, maintainer
 from .classify import classify_grain
 from .pgdtools import PresolarGrains
+from ._version import __version__
 
-__all__ = ["PresolarGrains", "classify_grain", "data", "maintainer"]
+__all__ = ["PresolarGrains", "classify_grain", "data", "maintainer", "__version__"]
 
 print("Really early dev version... user beware!")
 
 # Package information
-__version__ = get_version(root="..", relative_to=__file__)
 
 __title__ = "pgdtools"
 __description__ = "Read and interact with the presolar grain database with python."
